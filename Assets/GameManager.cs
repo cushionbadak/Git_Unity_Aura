ae16b511d7fac00ebc479eec41ca86f5e12450d6
cudak <jsbzwork@gmail.com>
2015-07-04 22:50:06 +0900

    GameManager 바뀜
    고려할 게 점점 많아지고 있다....
    -player가 만든 attack이 enemy에 맞는 경우
    enemy가 만든게 enemy가 맞는 경우
    
    때문이다.

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -14,9 +14,16 @@ public class GameManager : MonoBehaviour {
 		//enemy가 맞을 할 Attack객체인 경우.
 		//player상태를 고려해야 해서 코드가 길어진다.
 		if(hitThing.tag == "enemy" && attk.isAttackToEnemy) {
-			giveDamageToEnemy (hitThing, attk);
+			//플레이어가 만든 attack 이 enemy에 맞는 경우.
+			if(attk.isAttackFromPlayer)
+				giveDamageToEnemyFromPlayer (hitThing, attk);
+			//enemy가 만든 attack이 enemy에 맞는 경우.
+			//메소드 이름이 좀 이상해도 넘어가자.
+			else giveDamageToPlayer (hitThing, attk);
 		}
+
 		//player가 맞을 Attack 객체인 경우.
+		//player가 만든 attack이 player를 때릴 리가 없다고 본다.
 		//고려할것이 적다.
 		if(hitThing.tag == "player" && attk.isAttackToPlayer) {
 			giveDamageToPlayer (hitThing, attk);
@@ -34,8 +41,15 @@ public class GameManager : MonoBehaviour {
 	}
 	*/
 
-	public void giveDamageToEnemy(CharacterSkeleton hitThing, AttackSkeleton attk){
-
+	public void giveDamageToEnemyFromPlayer(CharacterSkeleton hitThing, AttackSkeleton attk){
+		float tmpDamage = attk.damage * playerDamageBuff ();
+		//이것도 아직 playerDamageBuff() 매개변수 종류를 모르겠어서 모르겠다.
+		//playerDamageBuff()가 구체적으로 완성되면 적어야한다.
+		float tmp = hitThing.currentHP - tmpDamage;
+		if (tmp > 0)
+			hitThing.currentHP = tmp;
+		else
+			hitThing.Die ();
 	}
 	public void giveDamageToPlayer(CharacterSkeleton hitThing, AttackSkeleton attk){
 		float tmp = hitThing.currentHP - attk.damage;
@@ -43,6 +57,13 @@ public class GameManager : MonoBehaviour {
 			hitThing.currentHP = tmp;
 		else
 			hitThing.Die ();
+	}
+	public static float playerDamageBuff(){
+		//player 객체가 매개변수로 들어와야하는데...이걸 어쩌지....?
+		//abstract class를 상속받는 interface를 만들어야하나....?
+		//아냐 호출 자체가 어려워.....이런!젠장! 이건 내일 물어봐야지.
+		//일단 return 0.0f 간다.
+		return 0.0f;
 	}
 }
 
@@ -54,7 +75,7 @@ public abstract class CharacterSkeleton : MonoBehaviour{
 	public float originalSpeed;
 	public float currentSpeed;
 	//마찬가지로 모든 캐릭터들이 가져야 할 기본적인 메소드들.
-	//캐릭터가 vector3 힘을 받아야한다.
+	//넉백은 캐릭터가 vector3 힘을 받아야한다.
 	public abstract void getKnockBack (Vector3 moveVector);
 	public abstract void getStun(float time);
 	public abstract void getSnare(float time);
@@ -91,4 +112,5 @@ public abstract class AttackSkeleton : MonoBehaviour{
 	public float snareTime;
 	public bool isAttackToPlayer;
 	public bool isAttackToEnemy;
+	public bool isAttackFromPlayer;
 }
