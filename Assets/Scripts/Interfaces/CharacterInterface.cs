eb4af63fec35bf88f0d71160de6e71eca5c775e8
cudak <jsbzwork@gmail.com>
2015-07-05 22:30:00 +0900

    인터페이스 만드는중

---
+++ Assets/Scripts/Interfaces/CharacterInterface.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface CharacterInterface {
+	float maxHP			{ get; set;	}
+	float currentHP		{ get; set;	}
+	int level			{ get; set;	}
+	float originalSpeed { get; set;	}
+	float currentSpeed	{ get; set;	}
+	void haveKnockBack(Vector3 moveVector);
+	void haveStun(float time);
+	void haveSnare(float time);
+	void Die();
+}
