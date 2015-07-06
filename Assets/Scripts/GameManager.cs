4f5dd07ec89ec6d1539ae293809280cd19ee22dc
cudak <jsbzwork@gmail.com>
2015-07-06 13:22:59 +0900

    GameManager - attackToPlayer create
    and name changed - In CharacterInterface,
    haveKnockBack -> haveKnockback
    .

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,5 +2,25 @@
 using System.Collections;
 
 public class GameManager : MonoBehaviour {
+	public void attackToPlayer(AttackInterface attk, PlayerInterface player){
+		//player가 효과를 받는 attack이 일어났을 때 일어날 일들 중
+		//데미지, 넉백, 기절, 경직 정보를 읽어서 player에게 넘겨준다.
+		//만약 데미지를 받은 뒤 플레이어가 죽을 체력이면 Die함수를 호출한다.
+		//스턴과 경직 둘 다 있는 경우 스턴만 적용된다.
+		float tempHP = player.currentHP - attk.damage;
+		player.currentHP = (tempHP <= 0) ? 0.0f : tempHP;
+		if (tempHP <= 0)
+			player.Die ();
+		else {
+			if(attk.knockbackVector.magnitude > 0.0f)
+				player.haveKnockback(attk.knockbackVector);
+			if(attk.stunTime > 0.0f)
+				player.haveStun(attk.stunTime);
+			else if(attk.snareTime > 0.0f)
+				player.haveSnare (attk.snareTime);
+		}
+	}//attackToPlayer End.
 
+	//public void attckToEnemy(AttackInterface attk, CharacterInterface character){
+	//}
 }
--- Assets/Scripts/Interfaces/CharacterInterface.cs
+++ Assets/Scripts/Interfaces/CharacterInterface.cs
@@ -7,7 +7,7 @@ public interface CharacterInterface {
 	int level			{ get; set;	}
 	float originalSpeed { get; set;	}
 	float currentSpeed	{ get; set;	}
-	void haveKnockBack(Vector3 moveVector);
+	void haveKnockback(Vector3 moveVector);
 	void haveStun(float time);
 	void haveSnare(float time);
 	void Die();
