ee3113c6bd578d4f1829bf55d05f2424c769c14b
cudak <jsbzwork@gmail.com>
2015-07-06 11:36:14 +0900

    Create attackInterface & BuffInterface

--- Assets/Scripts/Interfaces/AttackInterface.cs
+++ Assets/Scripts/Interfaces/AttackInterface.cs
@@ -2,5 +2,9 @@
 using System.Collections;
 
 public interface AttackInterface {
-
+	float damage	{ get; set;}
+	Vector3 knockbackVector { get; set;}//넉백의 크기.
+	float stunTime	{ get; set;}	//기절시간. 기절동안에는 오오라가 사라진다
+	float snareTime	{ get; set;}	//경직시간. 경직동안에는 오오라가 사라지지 않는다.
+	float speed		{ get; set;}	//객체의 이동속력.
 }
