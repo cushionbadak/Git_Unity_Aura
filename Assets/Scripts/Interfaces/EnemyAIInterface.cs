54c574286e794cc2d75a9ba1e3d1a2f0953d3a71
soltree12 <soltree12@naver.com>
2015-07-09 15:18:27 +0900

    Add Enemy Interfaces

---
+++ Assets/Scripts/Interfaces/EnemyAIInterface.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+interface EnemyAIInterface : EnemyScriptInterface
+{
+    public enum ENEMY_BUFF
+    {
+        STUN,
+        SLOW,
+        STUTTERING
+    };
+
+    void GiveBuff(EnemyAIInterface.ENEMY_BUFF buffnum, float rate, float time)
+    {
+
+    }
+}
+
