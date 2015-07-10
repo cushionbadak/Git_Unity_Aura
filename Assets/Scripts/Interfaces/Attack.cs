bf4ab11a804f74becefc3352947a2061cd310ebf
cudak <jsbzwork@gmail.com>
2015-07-10 18:23:27 +0900

    Interface to Class
    인터페이스로 구현되어있던것들을 클래스로 옮김.

---
+++ Assets/Scripts/Interfaces/Attack.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attack {
+	public float damage;				//데미지의 크기.
+	public Vector3 knockbackVector;		//넉백의 크기.
+	public float stunTime;				//기절시간. 기절동안에는 오오라가 사라진다
+	public float snareTime;				//경직시간. 경직동안에는 오오라가 사라지지 않는다.
+	public float speed;					//객체의 이동속력
+
+	public void pause(){}				//일시정지
+	public void resume(){}				//일시정지 해제
+}
