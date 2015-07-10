119cd40035a4cebcddf3fb339a3469b8d3302000
cudak <jsbzwork@gmail.com>
2015-07-10 17:10:41 +0900

    MakingSystem002 : Copy MakingSystem1 to Master..
    It's first commit in MakingSystem002

---
+++ Assets/Scripts/MakingSystem002/SceneScript/MainScreen.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			TotalManager.I.setCurrentScene("Test_Chapter1");
+			Debug.Log (TotalManager.I.getCurrentScene());
+		}
+	}
+}
---
+++ Assets/Scripts/MakingSystem002/TotalManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TotalManager : MonoBehaviour {
+	//Singleton
+	private static TotalManager uniqueInstance = null;
+	public static TotalManager I { get { return uniqueInstance; } }
+
+	void Awake(){
+		DontDestroyOnLoad (this.gameObject);
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		//Singleton
+		if (uniqueInstance == null)
+			uniqueInstance = this;
+		else
+			Destroy (this.gameObject);
+
+		TotalManager.I.currentScene = "Test_MainScreen";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private string[] Scenes = {"Test_MainScreen", "Test_Chapter1", "Test_Chapter2", "Test_Chapter3", "Test_Chapter4"};
+	private string currentScene;
+	public string getCurrentScene(){return currentScene;}
+	public void setCurrentScene(string value){
+		foreach (string i in Scenes) {
+			if(i.Equals (value)){
+				currentScene = value;
+				Application.LoadLevel (value);
+			}
+		}
+	}
+}
