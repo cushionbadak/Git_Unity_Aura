fb40628b28c7134b58a316653bd5a18a5675f5e3
joonlim92 <joonlim92@gmail.com>
2015-07-12 10:39:42 +0900

    menuadded
    start menu added
    click to next scene or to quit added

---
+++ Assets/Scripts/UI/menuscript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class menuscript : MonoBehaviour {
+
+	public Canvas quitMenu;
+	public Button newGame;
+	public Button load;
+	public Button option;
+	public Button exit;
+
+	// Use this for initialization
+	void Start () {
+		quitMenu = quitMenu.GetComponent<Canvas> ();
+		newGame = newGame.GetComponent<Button> ();
+		exit = exit.GetComponent<Button> ();
+		load = load.GetComponent<Button> ();
+		quitMenu.enabled = false;
+	}
+
+	public void ExitPress(){
+		quitMenu.enabled = true;
+		newGame.enabled = false;
+		exit.enabled = false;
+	}
+
+	public void NoPress(){
+		quitMenu.enabled = false;
+		newGame.enabled = true;
+		exit.enabled = true;
+	}
+
+	public void StartLevel1(){
+		Application.LoadLevel (1);
+	}
+
+	public void ExitGame(){
+		Application.Quit ();
+	}
+
+	
+}
