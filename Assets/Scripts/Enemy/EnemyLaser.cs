25877d6dde97b953f09b33ac120fd4ddc94bf9db
poong <ventusmagus@gmail.com>
2015-07-14 19:20:01 +0900

    Add EnemyLaser.cs

--- Assets/Scripts/Enemy/EnemyLaser.cs
+++ Assets/Scripts/Enemy/EnemyLaser.cs
@@ -1,7 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class EnemyLaser : MonoBehaviour {
+public class EnemyLaser : Attack {
+
+    public float predelay_time = 1;
+    public float postdelay_time = 1;
+
+    private float timer = 0;
+    private bool player_inside = false;
+    private bool isHit = false;
+
+    private bool isPaused = false;
 
 	// Use this for initialization
 	void Start () {
@@ -9,7 +18,57 @@ public class EnemyLaser : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+    {
+        if (isPaused)
+            return;
+
+        timer += Time.deltaTime;
+
+        if (timer > predelay_time && isHit == false)
+        {
+            if (player_inside)
+            {
+                Attack();
+            }
+
+            isHit = true;
+        }
+
+        if (timer > predelay_time + postdelay_time)
+        {
+            Object.Destroy(gameObject, 0.3f);
+        }
 	}
+
+
+    void Attack()
+    {
+        Debug.Log(gameObject.name + ".EnemyLaser : Attack damage : " + damage);
+    }
+
+
+    void OnTriggerEnter(Collider col)
+    {
+        Debug.Log(col.gameObject.name);
+        if (col.gameObject.name == "PlayerCollider")
+            player_inside = true;
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.name == "PlayerCollider")
+            player_inside = false;
+    }
+
+
+    public override void pause()
+    {
+        isPaused = true;
+    }
+
+    public override void resume()
+    {
+        isPaused = false;
+    }
 }
