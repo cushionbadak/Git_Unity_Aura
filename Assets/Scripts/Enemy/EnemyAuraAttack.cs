28d66285ada03376da3b5cfa12b711d5e8d23cc7
poong <ventusmagus@gmail.com>
2015-07-14 19:00:22 +0900

    Remove parameter on EnemyAuraAttack.cs inherited from Attack.cs

--- Assets/Scripts/Enemy/EnemyAuraAttack.cs
+++ Assets/Scripts/Enemy/EnemyAuraAttack.cs
@@ -3,12 +3,6 @@ using System.Collections;
 
 public class EnemyAuraAttack : Attack
 {
-    public float damage = 10;
-    public Vector3 knockbackVector = new Vector3(0, 0, 0);
-    public float stunTime = 0;
-    public float snareTime = 0;
-    public float speed = 0;
-
     public float attack_cooldown = 1;
     private float attack_timer = 0;
     private bool player_inside = false;
