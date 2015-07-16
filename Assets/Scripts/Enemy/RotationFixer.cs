f34f79fba7815cf7a5023141e3d6491bd55c7a3e
cudak <jsbzwork@gmail.com>
2015-07-16 17:48:18 +0900

    Fix Enemy Rotating

---
+++ Assets/Scripts/Enemy/RotationFixer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotationFixer : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.localEulerAngles = new Vector3(90, 0, 0);
+	}
+}
