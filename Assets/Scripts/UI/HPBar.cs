3a84a1b473f82084736a41b6689e9e6e5582cbe6
JoonSe Lim <joonlim92@gmail.com>
2015-07-16 17:15:01 +0900

    hpexpbar add
    hp exp bar added
    saved as scene and prefab

---
+++ Assets/Scripts/UI/HPBar.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HPBar : MonoBehaviour
+{
+	public float currentHealth;
+	public Slider hpSlider;
+	bool damaged;
+	Character currentHealthPoint = new Character();
+
+
+	void Awake(){
+		hpSlider.maxValue = PlayerLevelData.I.Status [1].maxHP;
+		currentHealth = PlayerLevelData.I.Status [1].maxHP;
+	}
+	
+	void Update(){
+		if (damaged) {												//damaged must be fixed
+			currentHealth = currentHealthPoint.currentHP;
+			hpSlider.value = currentHealth;
+		}
+	}
+}
