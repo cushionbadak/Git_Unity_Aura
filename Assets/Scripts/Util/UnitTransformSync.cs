fc524ada3ed9bc7e736ccc67b7c9b1d3da639446
cudak <jsbzwork@gmail.com>
2015-07-20 19:04:25 +0900

    Move RotationFixer to Util folder and make transform syncronizer

---
+++ Assets/Scripts/Util/UnitTransformSync.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+// This Script Synchronize Parent's Transform with This object
+public class UnitTransformSync : MonoBehaviour 
+{
+    private bool hasParent = true;
+
+	// Use this for initialization
+	void Start () 
+    {
+        if (transform.parent == null)
+        {
+            hasParent = false;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (hasParent)
+        {
+            transform.parent.position = transform.position;
+        }
+	}
+}
