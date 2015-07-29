8560061b498e1be66cf7ef6f811b27912f915fdf
soltree12 <soltree12@naver.com>
2015-07-29 18:26:58 +0900

    세이브시스템 작성중

---
+++ Assets/Scripts/Map/DoorCloseControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorCloseControl : MonoBehaviour {
+    public GameObject MapManager;
+    public GameObject door;
+    public int RoomNum;
+    // Use this for initialization
+    void Start () {
+	  
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerExit(Collider col)
+    {
+        if(MapManager.GetComponent<MapManager>().getRoomStatus()[RoomNum]==false)
+        if(col.tag=="PlayerBody")
+        {
+            door.GetComponent<ChangeAlpha>().isTrans = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Map/MapManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapManager : MonoBehaviour {
+    public int CurrentChapter;
+    public bool[] chap1 = new bool[8];//깨면 true
+    public GameObject[] chap1Doors = new GameObject[8];
+
+	// Use this for initialization
+	void Start () {
+        for(int i=0;i<chap1.Length;i++)
+        {
+            chap1[i] = false;
+        }
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public void DoorOpen(int i)
+    {
+        switch(CurrentChapter)
+        {
+            case 1:
+                {
+                    chap1Doors[i].GetComponent<ChangeAlpha>().isTrans = true;
+                    break;
+                }
+
+        }
+    }
+
+    public bool[] getRoomStatus()
+    {
+        switch(CurrentChapter)
+        {
+            case 1:
+                {
+                    return chap1;
+                }
+            default:
+                return null;
+        }
+    }
+}
