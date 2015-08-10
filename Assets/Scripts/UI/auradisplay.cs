a0d1c55add3163a519c4b1600b805b4d2d0a531c
joonlim92 <joonlim92@gmail.com>
2015-08-10 17:35:36 +0900

    escUI제작
    escUI제작

---
+++ Assets/Scripts/UI/auradisplay.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class auradisplay : MonoBehaviour {
+	
+	public GameObject player;
+	Player pl;
+	// Use this for initialization
+	void Start () {
+		pl = player.GetComponent<Player>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		string str = "오오라의 크기:" + pl.AuraRange.ToString ();
+		GetComponent<Text>().text = str;
+	}
+}
