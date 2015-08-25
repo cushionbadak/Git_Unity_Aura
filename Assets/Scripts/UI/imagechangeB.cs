ff0f25863a4d9490fc89ee9d25e59213448463f4
joonlim92 <joonlim92@gmail.com>
2015-08-25 22:31:03 +0900

    ui skillchange
    skillchange

---
+++ Assets/Scripts/UI/imagechangeB.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class imagechangeB : MonoBehaviour {
+	public GameObject skillupdate;
+	public Sprite knockback;
+	public Sprite laser;
+	public Sprite nothing;
+	public Sprite teleport;
+	public Sprite tripleshock;
+	public Sprite whindbiting;
+	public Sprite spinningcross;
+	public Sprite shugo;
+	PlayerSkills _skillselect = null;
+	
+	
+	// Use this for initialization
+	void Start () {
+		_skillselect = skillupdate.GetComponent<PlayerSkills> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (_skillselect._skill_2 == PlayerSkills.skillSet.Knockback) {
+			this.gameObject.GetComponent<Image> ().sprite = knockback;
+		}
+		if (_skillselect._skill_2 == PlayerSkills.skillSet.Laser) {
+			this.gameObject.GetComponent<Image> ().sprite = laser;
+		}
+		if (_skillselect._skill_2 == PlayerSkills.skillSet.Nothing) {
+			this.gameObject.GetComponent<Image> ().sprite = nothing;
+		}
+		if (_skillselect._skill_2 == PlayerSkills.skillSet.Teleport) {
+			this.gameObject.GetComponent<Image> ().sprite = teleport;
+		}
+		if (_skillselect._skill_2 == PlayerSkills.skillSet.TripleShock) {
+			this.gameObject.GetComponent<Image> ().sprite = tripleshock;
+		}
+		if (_skillselect._skill_2 == PlayerSkills.skillSet.WindBitingSnowBall) {
+			this.gameObject.GetComponent<Image> ().sprite = whindbiting;
+		}
+		if (_skillselect._skill_2 == PlayerSkills.skillSet.SpinningCross) {
+			this.gameObject.GetComponent<Image> ().sprite = spinningcross;
+		}
+		if (_skillselect._skill_2 == PlayerSkills.skillSet.ShugokuOokiidesu) {
+			this.gameObject.GetComponent<Image> ().sprite = shugo;
+		}
+	}
+}
---
+++ Assets/Scripts/UI/skillchange/skillchangetower.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class skillchangetower : MonoBehaviour {
+	public Button button;
+	public GameObject skillbringin;
+	PlayerSkills _skillselect;
+	
+	// Use this for initialization
+	void Start () {
+		_skillselect = skillbringin.GetComponent<PlayerSkills> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)){
+			_skillselect._skill_1 = PlayerSkills.skillSet.WindBitingSnowBall;
+		}
+	}
+}
